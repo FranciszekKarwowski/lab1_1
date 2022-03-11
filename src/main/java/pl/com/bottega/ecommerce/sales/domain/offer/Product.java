91bde1bdbfb9cd727295fcb14cfdfbd2c1d049da
FranciszekKarwowski <96189378+FranciszekKarwowski@users.noreply.github.com>
2022-03-11 18:25:03 +0100

    Added Product.java class

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Product.java
@@ -0,0 +1,60 @@
+package pl.com.bottega.ecommerce.sales.domain.offer;
+
+import java.util.Date;
+
+public class Product {
+    private String id;
+    private String name;
+    private String type;
+    private Date snapshotDate;
+
+    private Money price;
+
+    public Product(String id, String name, String type, Date snapshotDate, Money price) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.snapshotDate = snapshotDate;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Date getSnapshotDate() {
+        return snapshotDate;
+    }
+
+    public void setSnapshotDate(Date snapshotDate) {
+        this.snapshotDate = snapshotDate;
+    }
+
+    public Money getPrice() {
+        return price;
+    }
+
+    public void setPrice(Money price) {
+        this.price = price;
+    }
+}
